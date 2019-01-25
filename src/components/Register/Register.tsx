b72b2288547838cf8559fd5a4baf25a44123a066
NancySchmeisser <nancy.schmeisser@netzalist.de>
2019-01-25 14:12:37 +0100

    feat: added Register.tsx, Register.test.tsx

--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -1,34 +1,15 @@
 import React, { Component } from 'react';
-import './Login.css';
+
 import { timingSafeEqual } from 'crypto';
-import Register from '../Register/Register';
 
-class Login extends Component<any, any>{
-    constructor(props: any){
-        super(props);
-
-        this.state= {
-            currentEmail: "",
-            currentPassword: "",
-            currentMessage: "Please enter something",
-        }
-    }
-
-    handleTryLogIn = () => {
-
-        if( this.state.currentEmail === "nancy" && this.state.currentPassword === "nancy") {
-            this.props.handleLogIn(this.state.currentEmail);
-        } else {
-            this.setState( { currentMessage: "Wrong username or password" });
-        }
-    }
-
+class Register extends Component<any, any>{
+   
     render() {
         return (
-            <div className="login">
+            <div className="Register">
                 <div className="card">
                     <div className="card-header">
-                        <h5>Login</h5>
+                        <h5>Register</h5>
                     </div>
                     <div className="card-body">
                         <div className="container">
@@ -37,7 +18,7 @@ class Login extends Component<any, any>{
                                     Email
                                 </div>
                                 <div className="col">
-                                    <input type="text" className="form-control" placeholder="Please enter email" value={this.state.currentEmail} onChange={ (event) => { this.setState ({ currentEmail: event.target.value}) }} />
+                                  
                                 </div>
 
                             </div>
@@ -46,20 +27,18 @@ class Login extends Component<any, any>{
                                     Password
                                 </div>
                                 <div className="col">
-                                    <input type="password" className="form-control" placeholder="Please enter password" value={this.state.currentPassword} onChange={ (event) => { this.setState ({ currentPassword: event.target.value}) }}/>
-                                </div>
-                                </div>
+                                  
 
                             <div className="row">
                                 <div className="col">
-                                    <button onClick={ this.handleTryLogIn} className="btn btn-link float-right">Login</button>
+                                    <button  className="btn btn-link float-right">Register</button>
                                 </div>
                             </div>
 
                             <div className="row">
                                 <div className="col">
                                     <div className="alert alert-danger" role="alert">
-                                        {this.state.currentMessage}
+                                      
                                     </div>
                                 </div>
                             </div>
@@ -76,8 +55,10 @@ class Login extends Component<any, any>{
                     </div>
                 </div>
             </div>
+            </div>
+            </div>
         );
     }
 }
 
-export default Login;
+export default Register;
